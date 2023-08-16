b7ee0d855d0175c6aa30ee0f330782962c3b75b6
PiQuark6046 <piquark6046@proton.me>
2023-08-16 09:04:22 +0000

    feat: create exception rule generator

---
+++ dist/scripts.js
@@ -0,0 +1,34 @@
+"use strict";
+let DomainsMap = new Map([
+    ['womenonweb', 'womenonweb.org'],
+    ['pornhub', 'pornhub.com'],
+    ['hitomi', 'hitomi.la'],
+    ['xvideos', 'xvideos.com']
+]);
+function GenerateExceptionRule() {
+    let Domains = new Array();
+    document.querySelectorAll('#options input').forEach(function (element) {
+        if (element.checked)
+            Domains.push(DomainsMap.get(element.id));
+    });
+    Domains = Domains.concat(document.querySelector('#options textarea#others').value.split('\n'));
+    if (Domains[Domains.length - 1] === '')
+        Domains.pop();
+    let Rule = !Domains.length ? '@@*$stealth=dpi' : `@@*$stealth=dpi,domains=${Domains.join(',')}`;
+    document.querySelector('code').innerText = Rule;
+}
+document.querySelectorAll('#options input').forEach(function (elmeent) {
+    elmeent.addEventListener('change', function () {
+        if (getComputedStyle(document.querySelector('textarea#others')).getPropertyValue('border-color') !== 'rgb(255, 0, 0)')
+            GenerateExceptionRule();
+    });
+});
+document.querySelector('textarea#others').addEventListener('input', function () {
+    if (/([a-z0-9-\.]+\.[a-z0-9]+\n?)+/.test(document.querySelector('textarea#others').value)) {
+        document.querySelector('textarea#others').style.removeProperty('border-color');
+        GenerateExceptionRule();
+    }
+    else {
+        document.querySelector('textarea#others').style.borderColor = 'red';
+    }
+});
---
+++ src/scripts.ts
@@ -0,0 +1,32 @@
+let DomainsMap = new Map<string, string>([
+  ['womenonweb', 'womenonweb.org'],
+  ['pornhub', 'pornhub.com'],
+  ['hitomi', 'hitomi.la'],
+  ['xvideos', 'xvideos.com']
+])
+
+function GenerateExceptionRule() {
+  let Domains: Array<string> = new Array()
+  document.querySelectorAll('#options input').forEach(function (element:HTMLInputElement) {
+    if (element.checked) Domains.push(DomainsMap.get(element.id))
+  })
+  Domains = Domains.concat((document.querySelector('#options textarea#others') as HTMLTextAreaElement).value.split('\n'))
+  if (Domains[Domains.length - 1] === '') Domains.pop()
+  let Rule = !Domains.length ? '@@*$stealth=dpi' : `@@*$stealth=dpi,domains=${Domains.join(',')}`
+  document.querySelector('code').innerText = Rule
+}
+
+document.querySelectorAll('#options input').forEach(function(elmeent) {
+  elmeent.addEventListener('change', function() { 
+    if (getComputedStyle(document.querySelector('textarea#others')).getPropertyValue('border-color') !== 'rgb(255, 0, 0)') GenerateExceptionRule()
+  })
+})
+
+document.querySelector('textarea#others').addEventListener('input', function() {
+  if (/([a-z0-9-\.]+\.[a-z0-9]+\n?)+/.test((document.querySelector('textarea#others') as HTMLTextAreaElement).value)) {
+    (document.querySelector('textarea#others') as HTMLTextAreaElement).style.removeProperty('border-color')
+    GenerateExceptionRule()
+  } else {
+    (document.querySelector('textarea#others') as HTMLTextAreaElement).style.borderColor = 'red'
+  }
+})
